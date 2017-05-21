00edbb9df004332e5d35d9f1a243e473b56e01c5
motty <a.yoshimasa@gmail.com>
2017-05-21 11:18:49 +0900

    add sound file

--- GottaniRPG/Form1.cs
+++ GottaniRPG/Form1.cs
@@ -12,9 +12,83 @@ namespace GottaniRPG
 {
     public partial class Form1 : Form
     {
+        SoundPlayer soundPlayer = new SoundPlayer();
+        Dictionary<int, string> nameList = new Dictionary<int, string>();
         public Form1()
         {
             InitializeComponent();
+
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //再生終了イベントが不要なら↓の2行は不要
+            soundPlayer.Init(this);
+            soundPlayer.PlayEnd += SoundPlayer_PlayEnd; //再生終了イベント
+
+            OpenSound("sound/coin07.mp3", "coin");
+            OpenSound("sound/crrect_answer3.mp3", "crrect");
+            OpenSound("sound/diving1.mp3", "diving");
+        }
+
+        void OpenSound(string file, string name)
+        {
+            var id = soundPlayer.Open(file, name);
+            nameList.Add(id, name); //idと名前を保存
+        }
+
+        private void SoundPlayer_PlayEnd(int id)
+        {
+            var name = nameList[id]; //idから名前を取得
+            switch (name)
+            {
+                case "crrect": //1回でクローズ
+                    soundPlayer.Close(name);
+                    nameList.Remove(id);
+                    break;
+                case "diving": //ループ
+                    soundPlayer.Seek(name, "0");
+                    soundPlayer.Play(name);
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //コイーン！を再生
+            soundPlayer.Seek("coin", "0");
+            soundPlayer.Play("coin");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //正解！を1回だけ再生
+            soundPlayer.Play("crrect");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //ブクブクブクをループ再生
+            soundPlayer.Play("diving");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //ステータス表示
+            var sb = new StringBuilder();
+            foreach (var name in nameList.Values)
+            {
+                var status = soundPlayer.Status(name);
+                var pos = soundPlayer.Position(name);
+                sb.AppendLine($"{name} {status} {pos}");
+            }
+            //textBox1.Text = sb.ToString();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //忘れずに開放処理
+            soundPlayer.CloseAll();
+            soundPlayer.Dispose();
         }
     }
 }
---
+++ GottaniRPG/SoundPlayer.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GottaniRPG
+{
+    public class SoundPlayer : IDisposable
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringW", CharSet = CharSet.Unicode)]
+        private static extern int mciSendString(string lpszCommand, StringBuilder lpszReturnString, int cchReturn, IntPtr hwndCallback);
+
+        [DllImport("winmm.dll", EntryPoint = "mciGetDeviceIDW", CharSet = CharSet.Unicode)]
+        private static extern uint mciGetDeviceID(string lpstrName);
+
+        public event Action<int> PlayEnd;
+
+        IntPtr handle = IntPtr.Zero;
+        SoundPlayerWindow win = null;
+
+        public SoundPlayer() { }
+
+        public void Init(Control parent)
+        {
+            handle = parent.Handle;
+            win = new SoundPlayerWindow();
+            win.AssignHandle(handle);
+            win.PlayEnd += id => PlayEnd?.Invoke(id);
+        }
+
+        public int GetDeviceID(string name)
+        {
+            return (int)mciGetDeviceID(name);
+        }
+
+        public int Open(string file, string name)
+        {
+            var cmd = "";
+            switch (Path.GetExtension(file))
+            {
+                case ".mp3":
+                    cmd = $"open \"{file}\" alias {name} type MPEGVideo";
+                    break;
+                case ".wav":
+                    cmd = $"open \"{file}\" alias {name} type waveaudio";
+                    break;
+                case ".mid":
+                    cmd = $"open \"{file}\" alias {name} type sequencer";
+                    break;
+                default:
+                    cmd = $"open \"{file}\" alias {name}";
+                    break;
+            }
+
+            if (0 > mciSendString(cmd, null, 0, handle))
+                return -1;
+
+            return GetDeviceID(name);
+        }
+
+        public int Play(string name)
+        {
+            var cmd = $"play {name} notify";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public int Play(string name, string from, string to)
+        {
+            var cmd = $"play {name} from {from} to {to} notify";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public int Stop(string name)
+        {
+            var cmd = $"stop {name}";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public int Close(string name)
+        {
+            var cmd = $"close {name}";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public int CloseAll()
+        {
+            var cmd = $"close all";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public int Pause(string name)
+        {
+            var cmd = $"pause {name}";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public int Resume(string name)
+        {
+            var cmd = $"resume {name}";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public int Seek(string name, string pos)
+        {
+            var cmd = $"seek {name} to {pos}";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public string Status(string name)
+        {
+            var sb = new StringBuilder(100);
+            var cmd = $"status {name} mode";
+            mciSendString(cmd, sb, sb.Capacity, handle);
+            return sb.ToString();
+        }
+
+        public string Position(string name)
+        {
+            var sb = new StringBuilder(100);
+            var cmd = $"status {name} position";
+            mciSendString(cmd, sb, sb.Capacity, handle);
+            return sb.ToString();
+        }
+
+        public string TimeFormat(string name)
+        {
+            var sb = new StringBuilder(100);
+            var cmd = $"status {name} time format";
+            mciSendString(cmd, sb, sb.Capacity, handle);
+            return sb.ToString();
+        }
+
+        public int SetTimeFormat(string name, string format)
+        {
+            var cmd = $"set {name} time format {format}";
+            return mciSendString(cmd, null, 0, handle);
+        }
+
+        public string Length(string name)
+        {
+            var sb = new StringBuilder(100);
+            var cmd = $"status {name} length";
+            mciSendString(cmd, sb, sb.Capacity, handle);
+            return sb.ToString();
+        }
+
+        public void Dispose()
+        {
+            if (win != null)
+                win.ReleaseHandle();
+            CloseAll();
+        }
+
+        protected class SoundPlayerWindow : NativeWindow
+        {
+            public event Action<int> PlayEnd;
+
+            protected override void WndProc(ref Message m)
+            {
+                const int MM_MCINOTIFY = 0x3B9;
+                const int MCI_NOTIFY_SUCCESSFUL = 1;
+                switch (m.Msg)
+                {
+                    case MM_MCINOTIFY:
+                        if ((int)m.WParam == MCI_NOTIFY_SUCCESSFUL)
+                        {
+                            PlayEnd?.Invoke((int)m.LParam);
+                        }
+                        break;
+                }
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
