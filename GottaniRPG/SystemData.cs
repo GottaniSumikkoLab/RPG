6f6cfb0ec9cc560a3ff81505ce159a0281ab8e98
motty <a.yoshimasa@gmail.com>
2017-06-04 15:20:52 +0900

    add SystemData.cs

---
+++ GottaniRPG/SystemData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GottaniRPG
+{
+    public static class SystemData
+    {
+        List<Bitmap> bmp = new List<Bitmap>();
+        public static void LoadFile()
+        {
+
+        }
+    }
+}
