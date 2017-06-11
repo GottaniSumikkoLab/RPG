dcfff2f824aef5bd75fdaf48bf2586f5feab86da
motty <a.yoshimasa@gmail.com>
2017-06-11 13:14:35 +0900

    MapEditer.csの作成

---
+++ GottaniRPG/MapEditer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GottaniRPG
+{
+    public partial class MapEditer : Form
+    {
+        public MapEditer()
+        {
+            this.Width = 640;//windowsize x
+            this.Height = 480;//windowsize y
+            this.StartPosition = FormStartPosition.CenterScreen;//画面中央表示
+            this.BackColor = Color.FromArgb(0, 0, 0);//背景色
+        }
+    }
+}
--- GottaniRPG/Program.cs
+++ GottaniRPG/Program.cs
@@ -16,7 +16,7 @@ namespace GottaniRPG
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MapEditer());
         }
     }
 }
