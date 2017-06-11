86ce55d6d0e356555d3fc5b1d1be8f0d35c4ca28
裕太 張替 <haiyuushuu.ut@outlook.com>
2017-06-11 15:16:38 +0900

    giridline

--- GottaniRPG/MapEditer.cs
+++ GottaniRPG/MapEditer.cs
@@ -22,6 +22,7 @@ namespace GottaniRPG
             this.BackColor = Color.FromArgb(0, 0, 0);//背景色
 
             Paint += new PaintEventHandler(MyHandler);
+            Paint += new PaintEventHandler(GridLine);
         }
 
         private void MyHandler(object sender, PaintEventArgs e)
@@ -30,6 +31,22 @@ namespace GottaniRPG
             for (int i = 0; i < MapEditSystemData.pic_data[1].mapChipArray.Length; i++)
             {
                 g.DrawImage(MapEditSystemData.pic_data[1].mapChipArray[i], new PointF(48f * i, 0));
+            }
+        }
+
+        private void GridLine(object sender, PaintEventArgs e)
+        {
+            Graphics verticalline = e.Graphics;
+            Graphics horizonalline = e.Graphics;
+
+            for(int j = 0; j < this.Height / 48; j++)
+            {
+                horizonalline.DrawLine(new Pen(Color.White), 0, j*48, this.Width, j*48);
+            }
+
+            for (int i = 0; i < this.Width / 48; i++)
+            {
+                verticalline.DrawLine(new Pen(Color.White), i * 48, 0, i * 48, this.Height);
             }
         }
     }
