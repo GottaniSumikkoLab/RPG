7f9036bac21327fe9ead1e9660a4b073b23f4c7f
motty <a.yoshimasa@gmail.com>
2017-06-11 15:07:07 +0900

    Mapの読み込みが完成

--- GottaniRPG/Map.cs
+++ GottaniRPG/Map.cs
@@ -11,17 +11,20 @@ namespace GottaniRPG
     {
         private int pic_size_x = 48;
         private int pic_size_y = 48;
+        private string name;
         public Bitmap[] mapChipArray;
-        public Map(int x, int y, Bitmap pic)
+        public Map(int x, int y, string name)
         {
+            this.name = name;
             int sum = x * y;
+            Bitmap large_map = new Bitmap("tilesets/" + name + ".png");
             mapChipArray = new Bitmap[sum];
 
             for (int i = 0; i < sum; i++)
             {
                 mapChipArray[i] = new Bitmap(pic_size_x,pic_size_y);
                 Graphics graphics = Graphics.FromImage(mapChipArray[i]);
-                graphics.DrawImage(pic, new Rectangle(0, 0, pic_size_x, pic_size_y),
+                graphics.DrawImage(large_map, new Rectangle(0, 0, pic_size_x, pic_size_y),
                     new Rectangle(pic_size_x * (i % x), pic_size_y * (i / y), pic_size_x, pic_size_y), GraphicsUnit.Pixel);
             }
         }
--- GottaniRPG/MapEditSystemData.cs
+++ GottaniRPG/MapEditSystemData.cs
@@ -9,23 +9,43 @@ namespace GottaniRPG
 {
     class MapEditSystemData
     {
-        public const int pic_num = 2;
-        public const string Tilesets = "tilesets/";
-        public const string Dungeon_A = "Dungeon_A";
-        public const string Png = ".png";
-        public static Bitmap[] Dungeon_A_Arr = new Bitmap[pic_num];
+        public const int pic_num = 31;
 
         public static Map[] pic_data = new Map[pic_num];
 
         public static void LoadFile()
         {
-            for (int i = 0; i < Dungeon_A_Arr.Length; i++)
-            {
-                Dungeon_A_Arr[i] = new Bitmap(Tilesets + Dungeon_A + (i + 1) + Png);
-            }
-
-            pic_data[0] = new Map(16, 12, Dungeon_A_Arr[0]);
-            pic_data[1] = new Map(16, 12, Dungeon_A_Arr[1]);
+            pic_data[0]  = new Map(16, 12, "Dungeon_A1");
+            pic_data[1]  = new Map(16, 12, "Dungeon_A2");
+            pic_data[2]  = new Map(16, 15, "Dungeon_A4");
+            pic_data[3]  = new Map(8 , 16, "Dungeon_A5");
+            pic_data[4]  = new Map(16, 16, "Dungeon_B" );
+            pic_data[5]  = new Map(16, 16, "Dungeon_C" );
+            pic_data[6]  = new Map(16, 12, "Inside_A1" );
+            pic_data[7]  = new Map(16, 12, "Inside_A2" );
+            pic_data[8]  = new Map(16, 15, "Inside_A4" );
+            pic_data[9]  = new Map(8 , 16, "Inside_A5" );
+            pic_data[10] = new Map(16, 16, "Inside_B"  );
+            pic_data[11] = new Map(16, 16, "Inside_C"  );
+            pic_data[12] = new Map(16, 12, "Outside_A1");
+            pic_data[13] = new Map(16, 12, "Outside_A2");
+            pic_data[14] = new Map(16, 8 , "Outside_A3");
+            pic_data[15] = new Map(16, 15, "Outside_A4");
+            pic_data[16] = new Map( 8, 16, "Outside_A5");
+            pic_data[17] = new Map(16, 16, "Outside_B" );
+            pic_data[18] = new Map(16, 16, "Outside_C" );
+            pic_data[19] = new Map(16, 15, "SF_Inside_A4");
+            pic_data[20] = new Map(16, 16, "SF_Inside_B");
+            pic_data[21] = new Map(16, 16, "SF_Inside_C");
+            pic_data[22] = new Map(16, 8 , "SF_Outside_A3");
+            pic_data[23] = new Map(16, 15, "SF_Outside_A4");
+            pic_data[24] = new Map(8 , 16, "SF_Outside_A5");
+            pic_data[25] = new Map(16, 16, "SF_Outside_B");
+            pic_data[26] = new Map(16, 16, "SF_Outside_C");
+            pic_data[27] = new Map(16, 12, "World_A1");
+            pic_data[28] = new Map(16, 12, "World_A2");
+            pic_data[29] = new Map(16, 16, "World_B");
+            pic_data[30] = new Map(16, 16, "World_C");
         }
     }
 }
