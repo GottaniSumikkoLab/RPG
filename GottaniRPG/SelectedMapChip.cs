d33872030e2ae222c66017afefb96f8b99995230
motty <a.yoshimasa@gmail.com>
2017-07-23 17:32:54 +0900

    a

---
+++ GottaniRPG/SelectedMapChip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GottaniRPG
+{
+    class SelectedMapChip
+    {
+        public Bitmap MapChip;
+        public string tileset_name;
+        public int MapChip_index;
+        public SelectedMapChip(Bitmap mapchip, string name, int index)
+        {
+            MapChip = mapchip;
+            tileset_name = name;
+            MapChip_index = index;
+        }
+    }
+}
